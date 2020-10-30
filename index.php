f3cbceabbd8c9ccfd1da40dee3d485422ee9d48e
schwazen <41826476+schwazen@users.noreply.github.com>
2020-10-30 12:59:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,13 +10,11 @@
         <img src='https://dogsnet.com/wp-content/uploads/2019/01/my-dog-ate-a-frog-long.jpg'>
         <img src='https://media.istockphoto.com/photos/frog-dog-picture-id467899032?k=6&m=467899032&s=170667a&w=0&h=cGJdYSkUFLBfc-ppfLgPhU4EnPaMIL15dLiVu08u4Cw='>
         <br>
-        <a href='https://userinyerface.com/game.html'>This is a Link</a>
+        <a href='https://userinyerface.com/game.html'>My favorite website EVER</a>
 
-        <h2>The unordered list</h2>
-        <ul>
-            <li>Google</li>
-            <li>Yahoo</li>
-            <li>Amazon</li>
-        </ul>
+        <h1>ปีที่แล้วผมมีโอกาสได้ไปงาน Open house ที่มอ.หาดใหญ่ ก็เดินกับเพื่อนมาประมาณ 4-5 คน <br>
+        ก็ชวนกันไปเข้าห้องน้ำ ตอนนั้นก็ไม่รู้ทาง ห้องน้ำจะเป็นแบบฝั่งขวาเป็นห้องน้ำชาย ฝั่งซ้ายห้องน้ำหญิง <br>
+        ด้วยความที่เคยไปครั้้งแรกบวกกับความรีบ แล้วป้ายห้องน้ำชาย-หญิงดันไปอยู่หลังประตู(ใครเป็นคนคิด) เดินเข้าฝั่งซ้ายอย่างมั่นใจทั้งหมู่คณะ <br>
+        บังเอิญมีกลุ่มผู้หญิง 5-6 คน เดินตามมา ไม่รู้ทางเหมือนกัน เลี้ยวเข้าห้องน้ำชายกันหมด แล้วทุกคนก็ยืนงง 3 วิก่อนจะวิ่งออกจากห้องน้ำ บันเทิงสิครับ</h1>
     </body>
 </html>
